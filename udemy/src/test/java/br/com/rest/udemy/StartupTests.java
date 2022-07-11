94cfbc7dbfd4c6b45add3fc33b079f8ee752272e
Renato Santana Brito <britotecst@outlook.com>
2022-07-11 00:16:34 -0300

    First steps in Java with Spring boot 3

---
+++ udemy/src/test/java/br/com/rest/udemy/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.rest.udemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
